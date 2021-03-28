cfbb34ccafc2b9eabd372d7b80d2c2ebd572f01e
Dimitris Karakatsanis <narcolic832@gmail.com>
2021-03-28 23:43:19 +0100

    Set up Data mapper (ORM) sln Create userdata class to get user information

---
+++ DKRDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,16 @@
+﻿using DKRDataManager.Library.Internal.DataAccess;
+using DKRDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace DKRDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string id)
+        {
+            var parameters = new { Id = id };
+
+            return new SqlDataAccess().LoadData<UserModel, dynamic>("dbo.spUserLookup", parameters, "DKRData");
+        }
+    }
+}
---
+++ DKRDataManager/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using DKRDataManager.Library.DataAccess;
+using DKRDataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DKRDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userID = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userID);
+        }
+    }
+}
