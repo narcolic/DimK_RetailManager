e42c97b1768f4422fbeec25636ba3d8ece05ac53
Dimitris Karakatsanis <narcolic832@gmail.com>
2021-04-04 20:40:23 +0100

    Replace Container ref with IoC

--- DKRDesktopUI/ViewModels/ShellViewModel.cs
+++ DKRDesktopUI/ViewModels/ShellViewModel.cs
@@ -7,18 +7,15 @@ namespace DKRDesktopUI.ViewModels
     {
         private readonly IEventAggregator _events;
         private SalesViewModel _salesViewModel;
-        private readonly SimpleContainer _container;
 
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesViewModel,
-            SimpleContainer container)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesViewModel)
         {
             _events = events;
             _salesViewModel = salesViewModel;
-            _container = container;
 
             _events.Subscribe(this);
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
